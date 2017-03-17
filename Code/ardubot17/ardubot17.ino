6c0ae71dc7fbb95f459b508d78fd620fb592db66
Felipe Kühne <fkuhne@pucrs.br>
2017-03-17 17:20:56 -0300

    Working on tokenization; Changing from NRF24 to Bluetooth.

--- Code/ardubot17/ardubot17.ino
+++ Code/ardubot17/ardubot17.ino
@@ -7,40 +7,17 @@
  * THIS IS FREE SOFTWARE. CHOOSE THE LICENSE THAT BEST FITS FOR YOU.
  */
 
-/* NRF24L01 comm based on https://arduino-info.wikispaces.com/Nrf24L01-2.4GHz-ExampleSketches#bm1 
- * and RF24 lib (https://github.com/TMRh20/RF24).
- * NRF24 pinout to Arduino
- *  1: GND
- *  2: VCC 3.3V !!! NOT 5V
- *  3: CE to Arduino pin 7 (D7)
- *  4: CSN to Arduino pin 8 (D8)
- *  5: SCK to Arduino pin 13 (SCK)
- *  6: MOSI to Arduino pin 11 (MOSI)
- *  7: MISO to Arduino pin 12 (MISO)
- *  8: INT (not used)
- *  
- *  L298N pinout to Arduino:
- *  5V jump in (9v bat powered)
- *  enA: D9 (purple)
- *  in1: D2 (yellow)
- *  in2: D3 (green)
- *  in3: D4 (brown)
- *  in4: D5 (red)
- *  enB: D10 (white)
+/* A good source of information about bluetooth modules with Arduino is here:
+ * http://www.martyncurrey.com/connecting-2-arduinos-by-bluetooth-using-a-hc-05-and-a-hc-06-pair-bind-and-link/
  */
 
- 
-#include <L298N.h>
-#include <RF24.h>
-#include <RF24_config.h>
-#include <nRF24L01.h>
-#include <printf.h>
+#include <SoftwareSerial.h>
 
 /*
  * Global constant definitions:
  */
 
-const int speedHisteresys = 100;
+const int speedHisteresys = 10;
 
 /* Pins used for motor 1: */
 const int enable1_pin = 9;
@@ -53,81 +30,85 @@ const int inA2_pin = 4;
 const int inB2_pin = 5;
 
 /*
- * Radio:
+ * Global stuff:
  */
 
-const int ce_pin = 7;
-const int csn_pin = 8;
-const int radioChannel = 108;
-byte addresses[][6] = {"Node1"}; // These will be the names of the "Pipes"
-typedef struct {
-	int x;
-	int y;
-	bool sw;
-} joystickData;
-
-joystickData joystick{0, 0, 0};
-
-/*
- * Global objects:
- */
+/* Connect the HC-05 TX to Arduino pin 2 RX and HC-05 RX to Arduino pin 3 TX
+ * through a voltage divider: 5V---( 1k )--[RX]--(2k)---GND */
+SoftwareSerial BTserial(btSerialRX_pin, btSerialTX_pin);
 
 DCMotor motor1(enable1_pin, inA1_pin, inB1_pin);
 DCMotor motor2(enable2_pin, inA2_pin, inB2_pin);
 L298N l298n(motor1, motor2);
 
-RF24 radio(ce_pin, csn_pin);
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 
 void setup()
 {
-  radio.begin(); /* Initialize the nRF24L01 Radio. */
-  radio.setChannel(radioChannel); /* 2.508 Ghz - Above most Wifi channels. */
-  radio.setDataRate(RF24_250KBPS); /* Fast enough, better range. */
-
-  /* Set the Power Amplifier Level low to prevent power supply related issues. */
-  radio.setPALevel(RF24_PA_LOW); /* RF24_PA_MAX */
-
-  /* Open reading pipe. */
-  radio.openReadingPipe(1, addresses[0]);
-
-  /* Start the radio listening for data. */
-  radio.startListening();
-
-  //radio.printDetails();
-
   Serial.begin(9600);
+  
+  BTSerial.begin(9600);
+}
+
+void waitCompleteSentence()
+{
+  while(stringComplete == false && BTSerial.available()) 
+  {
+    /* Get the new byte and add it to the inputString. */
+    inputString += (char)BTSerial.read();
+	
+    /* If the incoming character is a newline, set a flag so the main loop can
+	 * do something about it. Also, remove the trailing new line. */
+    if(inputString[inputString.length()] == '\n')
+	{
+	  inputString[inputString.length()] == '\0';
+      stringComplete = true;
+	}
+  }
 }
 
 void loop()
 {
-  //Serial.println("waiting...");
+  waitCompleteSentence();
   
-  if(radio.available())
-  {
-    while(radio.available())
-      radio.read(&joystick, sizeof(joystick));
+  char *token = strtok(inputString.c_str(), ",");
+  int digitalX = atoi(token);
+  token = strtok(NULL, ",");
+  int digitalY = atoi(token);
+  token = strtok(NULL, ",");
+  int switch = atoi(token);
+  
+  /* Debug: */
+  Serial.print("digitalX = "); Serial.println(digitalX);
+  Serial.print("digitalY = "); Serial.println(digitalY);
+  Serial.print("switch = "); Serial.println(switch);
+}
 
-    /* Debug only. */
-    Serial.print(joystick.x); Serial.print(" ");
-    Serial.print(joystick.y); Serial.print(" ");
-    Serial.println(joystick.sw); 
+ 
+void parseData() {
 
-	/* If joystick data is inside an hysteresis region, stop both motors. */
-    if((joystick.y >= (1023/2 - speedHisteresys)) &&
-       (joystick.y <= (1023/2 + speedHisteresys)))
-      l298n.setState(STOP);
+    // split the data into its parts
     
-	  else if(joystick.y < (1023/2 - speedHisteresys))
-	  {
-	    l298n.setDirection(FW);
-	    l298n.setSpeed(V1, W0);
-	    l298n.setState(RUN);
-  	}
-	  else if(joystick.y > (1023/2 + speedHisteresys))
-  	{
-	    l298n.setDirection(BW);
-	    l298n.setSpeed(V1, W0);
-  	  l298n.setState(RUN);
-	  }
-  }
+  char * strtokIndx; // this is used by strtok() as an index
+  
+  strtokIndx = strtok(receivedChars,",");      // get the first part - the string
+  strcpy(messageFromPC, strtokIndx); // copy it to messageFromPC
+  
+  strtokIndx = strtok(NULL, ","); // this continues where the previous call left off
+  integerFromPC = atoi(strtokIndx);     // convert this part to an integer
+  
+  strtokIndx = strtok(NULL, ","); 
+  floatFromPC = atof(strtokIndx);     // convert this part to a float
+
+}
+
+
+void showParsedData() {
+ Serial.print("Message ");
+ Serial.println(messageFromPC);
+ Serial.print("Integer ");
+ Serial.println(integerFromPC);
+ Serial.print("Float ");
+ Serial.println(floatFromPC);
 }
